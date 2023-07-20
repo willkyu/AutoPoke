018653db33a65ce30be25f5b5e0638ffcea97103
willkyu <87601487+willkyu@users.noreply.github.com>
2023-07-20 17:16:46 +0800

    Update mailTools.py

--- utils/mailTools.py
+++ utils/mailTools.py
@@ -6,18 +6,13 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 from PIL import Image
 
-# 邮件服务器地址,以下为网易邮箱
-pop3_server = 'pop.163.com'
-
-def sendMail(i=0, toMail='接收邮箱', subject='', picfilename='tempIMG.bmp'):
+def sendMail(i, toMail, mail_host, mail_user, mail_pass):
     mail = MIMEMultipart('related')
-    mail_host = "smtp.163.com"
-    mail_user = "发送邮箱"
-    mail_pass = "授权码"
+
     receivers = toMail
     
-    if subject=='':
-      subject=u'Got Shiny Pokemon in {} SLs!'.format(i)
+    picfilename='tempIMG.bmp'
+    subject=u'Got Shiny Pokemon in {} SLs!'.format(i)
 
     # 图像压缩处理
     # img = Image.open(picfilename)
@@ -45,8 +40,9 @@ def sendMail(i=0, toMail='接收邮箱', subject='', picfilename='tempIMG.bmp'):
     try:
         smtpObj = smtplib.SMTP_SSL(mail_host)
         smtpObj.login(mail_user,mail_pass)
+        # print(123)
         smtpObj.sendmail(mail_user,  receivers, mail.as_string())
-        # print (f"{receivers} 邮件发送成功")
+        print (f"{receivers} 邮件发送成功")
     except smtplib.SMTPException:
         print ("Error: 无法发送邮件")
         pass
