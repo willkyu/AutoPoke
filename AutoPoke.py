ab15e695906ac8cd9691e01842f101707b0fca2c
willkyu <87601487+willkyu@users.noreply.github.com>
2024-05-08 13:36:13 +0800

    Update AutoPoke.py

--- AutoPoke.py
+++ AutoPoke.py
@@ -1,11 +1,15 @@
 import flet
 from utils.home import Home
 
-def mainPage(page: flet.Page)->None:
-    page.title="AutoPoke v4.2.1 by willkyu"
+VERSION = "4.3"
+
+
+def mainPage(page: flet.Page) -> None:
+    page.title = "AutoPoke v{} by willkyu".format(VERSION)
     page.window_width = 720
     page.window_height = 300
     homePage = Home(page)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     flet.app(target=mainPage)
